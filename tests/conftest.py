f9be923ac523a86aaf2dd640d40c309a1155b646
Gosuto Inzasheru <gosuto@inzasheru.email>
2024-06-26 17:37:21 +0200

    fix: deprecate goerli

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,7 +7,7 @@ from bal_tools.subgraph import Subgraph
 from bal_tools.ecosystem import Aura
 from bal_tools.safe_tx_builder import SafeTxBuilder
 
-exempt_chains = ["fantom", "mode", "linea", "fraxtal"]
+exempt_chains = ["fantom", "mode", "linea", "fraxtal", "goerli"]
 chains = [chain for chain in list(AddrBook.chains["CHAIN_IDS_BY_NAME"]) if chain not in exempt_chains]
 
 
@@ -47,7 +47,7 @@ def safe_tx_builder(msig_name) -> SafeTxBuilder:
 def erc20_abi():
     with open("tests/abi/erc20.json", "r") as file:
         return json.load(file)
-    
+
 @pytest.fixture(scope="module")
 def bribe_market_abi():
     with open("tests/abi/bribe_market.json", "r") as file:
