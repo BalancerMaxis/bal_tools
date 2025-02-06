496705129d8bb80dbc91ca1f226421f9fa100882
Gosuto Inzasheru <gosuto@inzasheru.email>
2025-02-06 14:51:24 +0000

    chore: bump internal version

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = "0.1.16"
+VERSION = "0.1.17"
 DESCRIPTION = "Balancer Tools"
 LONG_DESCRIPTION = "Balancer Maxi helper and ecosystem tools"
 
